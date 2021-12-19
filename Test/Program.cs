3caf6877eb522c2ae79d8966d98b48446e1cd3bf
Nuanu-123 <89157371+Nuanu-123@users.noreply.github.com>
2021-12-19 19:41:46 +0000

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,56 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+using System.Threading;
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            //Share Skill Page
+            [Test]
+            public void EnterShareSkillTest()
+
+            {
+                test = extent.StartTest("Enter Share Skill");
+                ShareSkill shareSkillPage = new ShareSkill();
+                shareSkillPage.EnterShareSkill();
+                shareSkillPage.VerifyEnterShareSkill();
+            }
+            [Test]
+            public void Managelistingpage()
+
+            {
+                test = extent.StartTest("View Listing");
+                ManageListings manageListings = new ManageListings();
+                manageListings.ActiveToggleWorking();
+            }
+
+           [Test]
+            public void AddProfileDetails()
+            {
+                test = extent.StartTest("Add Profile");
+                Profile profile = new Profile();
+                profile.EditProfile();
+                profile.VerifyEditProfile();
+            }
+
+
+            [Test]
+            public void DeleteListingTest()
+            {
+                test = extent.StartTest("Delete Listing");
+                ManageListings deletelisting = new ManageListings();
+                deletelisting.DeleteListing();
+                deletelisting.VerifyDeleteListing();
+            }
+
+
+        }
+
+    }
+}
